c2c82e4132d1140ed2a3bfb216c2037220cd22dc
kittybagpipes <sparticus978@gmail.com>
2021-02-01 22:22:24 -0800

    deploy problems

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
   DATABASE_URL:
     process.env.DATABASE_URL ||
-    "postgresql://bradford:Simpson66!@localhost/langellium",
+    "postgresql://bradford:Simpson66!@localhost/langellium?ssl=true",
   JWT_SECRET: process.env.JWT_SECRET || "change-this-secret",
   JWT_EXPIRY: process.env.JWT_EXPIRY || "3h",
 };
